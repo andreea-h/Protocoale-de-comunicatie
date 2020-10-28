eb5c5db0929c19fd04c7c71e36d1fe930ae0d1ce
andreea-h <andreeahorovei@yahoo.com>
2020-10-28 20:58:01 +0200

    fisier checker

---
+++ Tema1-Router/info.py
@@ -0,0 +1,5 @@
+óãÛòhÒIfHè|0úFsRÑ%Ò6#pÆsqA8Ž%Î"Á»mžÔbw#á¦Vkö?¿0°ÞúÏòjV*äØ4 ¤÷SØ?6
+…ÐB?­úô–ÊuP­m¦:‚¶é¶*Þj0^‘¾Ý%Î]ÁãC/Ø—tí.Ù²
+ëS~Öì‡Úœ-ÿàÀG@oñ:À¤P¥ò¹Åü\ñã<ã±Ðø;äÕq¯„˜u“%t×±ñF>n…£­Ô 9CêÊ”nÑÂ¤»D¤ÁÐÞÛ¯<FÑ1jÖÊ‡n8— ¿úd@ƒø“®/Ú\‚Ÿœ‹‹É¹ˆ“ö4@1ÈíJû•Í†œšÄAêžXë°M?Ù•‹§
+fúà:©<,F#©üüÞ†Å§¬Ö£/É{Ë!Z8w¬ÍH'%ž-ÿéG‡YêÂâ:ò±ÓðÕÓ®PUn—.ÖZÔA =œ[Ž°5ÂíRôJ)bDÆi<Ï5/¤áécÑ^–»ç×õ&¢
+­ç\*X]*>Vaf€YHuü–wºÅ\úå€+µc‡\à½+ÈËÿß…#“š‡µÄY*ýv¬h	Þ`»k‹x²Ây¥„´v‘",å½ãáõéëæ´¬i?Ü5-}Vz†sŽæþéÙ§è5WÐyV&k3G-ÿò`Gqõ»¢¿Ü­$ÄPÑJùW¸4ôóTOC*šY»'üõ•!JñþâA@~)ó›	€Ìb!I46ïÙìYvÉ7V§ñ†öêƒXÈ+´BŽ³*Zî“ š•â²{øþ(¶{•Û¹T«>ÉÁaãÈÑþ»6t;lMVèžön-°jÌôó¦Ê: <s“ÕåÁ»­hg€°Ñ.¯Û¥à¡<‰sU%Òà^˜@Ÿü›VTã-ÿû0GX,MàËaˆÉ[!¤ƒmÒü’y‡Î
