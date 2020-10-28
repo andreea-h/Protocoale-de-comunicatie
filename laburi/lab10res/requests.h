26f85af8e3fe32860af73c21731cd143793b0a53
andreea-h <andreeahorovei@yahoo.com>
2020-10-28 21:40:04 +0200

    toate laboratoarele

--- laburi/lab10res/requests.h
+++
@@ -1,8 +0,0 @@
-#ifndef _REQUESTS_
-#define _REQUESTS_
-
-char *compute_get_request(char *host, char *url, char *url_params);
-char *compute_post_request(char *host, char *url, char *form_data);
-char *compute_delete_request(char *host, char *url);
-
-#endif
